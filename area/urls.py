c044b31be2d4d7960807863fad58894543ae9076
vlopez <victorl_222@hotmail.com>
2021-12-09 17:38:25 -0300

    cambios area empresa

--- area/urls.py
+++ area/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     path("agregar/",views.area_agregar,name='area_agregar'),
-    path("listar/",views.listar_area,name='area_listar'),
+    path("listar/",views.listar_area,name='listar_area'),
+    path('delete/<int:id_area>/',views.delete_area,name='area_delete'),
+    path('<int:id_area>/', views.AreaForm,name='area_update'), # get and post req. for update operation
+
 
 ]
--- empresa/views.py
+++ empresa/views.py
@@ -2,6 +2,8 @@ from .forms import EmpresaForm
 from django.db import models
 from .models import Empresa
 from django.shortcuts import render,redirect
+from django.contrib import messages
+
 
 # Create your views here.
 
@@ -10,23 +12,51 @@ def home(request):
     return render(request,'empresa/home.html')
 
 
-def empresa_agregar(request):
-    if request.method=="GET":
-        form =EmpresaForm()
-    
-        return render(request,'empresa/empresa_form.html',{'form':form})
 
 
+
+
+
+def empresa_agregar(request,id_empresa=0):
+    if request.method == "GET":
+        if id_empresa == 0 :
+            form = EmpresaForm()
+        else:
+            empresa = Empresa.objects.get(pk=id_empresa)
+            #invernadero = Invernadero.objects.filter(pk=id_invernadero).first()
+
+            form = EmpresaForm(instance=empresa)
+        return render(request, 'empresa/empresa_agregar.html', {'form': form})
     else:
-        form =EmpresaForm(request.POST)
+        if id_empresa == 0:
+            form = EmpresaForm(request.POST)
+        else:
+            empresa = Empresa.objects.get(pk=id_empresa)
+            form = EmpresaForm(request.POST,instance= empresa)
         if form.is_valid():
             form.save()
-        return redirect('/empresa/listar/')
-    
+            messages.add_message(request, messages.INFO, 'Agregado correctamente!.')
+
+        return redirect('/listar/')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 def listar_empresa(request):
-    empresas = Empresa.objects.all()
-    return render(request,'empresa/empresa_list.html',{'empresa':empresas})
+    context = {'listar_empresa': Empresa.objects.all()}
+    return render(request, "empresa/listar.html", context)
 
 
 
