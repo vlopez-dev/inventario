d7d4246b96db44746003331b4e45fc3f711a7520
vlopez <victorl_222@hotmail.com>
2021-12-13 17:59:06 -0300

    cambios movimiento

--- articulo/urls.py
+++ articulo/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path("agregar/",views.articulo_agregar,name='articulo_agregar'),
     path ('delete/<int:id_articulo>/',views.delete_articulo,name='articulo_delete'),
-    path ('movimiento/<int:id_articulo>/',views.movimiento,name='movimiento'),
 
+    path("movimiento/",views.movimiento,name='movimiento'),
 
     path("listar_articulo/",views.listar_articulo,name='articulo_listar'),
     path('<int:id_articulo>/', views.articulo_agregar,name='articulo_update'), # get and post req. for update operation
